b2fdbdc7c0983b9e8380ffa4298a8b49ee9c3062
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-24 18:00:14 -0300

    Concluindo etapa do curso

--- javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,18 +1,29 @@
-/*
-<table class="table table-hover table-bordered">
-        <thead>
-            <tr>
-                <th>DATA</th>
-                <th>QUANTIDADE</th>
-                <th>VALOR</th>
-                <th>VOLUME</th>
-            </tr>
-        </thead>
+class NegociacoesView {
+
+    constructor(elemento) {
         
-        <tbody>
-        </tbody>
-        
-        <tfoot>
-        </tfoot>
-    </table>
-*/
+        this._elemento = elemento;
+    }
+
+    template() {
+
+        return `
+            <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+            
+            <tbody>
+            </tbody>
+            
+            <tfoot>
+            </tfoot>
+            </table>     
+        `;
+    }
+}
