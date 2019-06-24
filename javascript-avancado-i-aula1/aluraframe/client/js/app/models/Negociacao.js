371caaa80fc8476ade205bfe374b7b2a3dd069ce
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-24 09:42:16 -0300

    criando get dos atributos para restringir acesso

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/Negociacao.js
@@ -2,14 +2,28 @@ class Negociacao {
  
     constructor(data, quantidade, valor) {
 
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
 
+        // O _ significa uma convenção que estas propiedades só pode ser acessadas
+        // pelos próprios métodos da classe e por ninguém de fora;  
     }
 
-    obtemVolume() {
+    getVolume() {
 
-        return this.quantidade * this.valor;
+        return this._quantidade * this._valor;
+    }
+
+    getData() {
+        return this._data;
+    }
+
+    getQuantidade() {
+        return this._quantidade;
+    }
+
+    getValor() {
+        return this._valor;
     }
 }
