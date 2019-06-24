a4c29c1855d36d15582d3514c0a045cad74639a6
JoaoPedroCordeiroC <joao.u@hotmail.com>
2019-06-24 11:42:46 -0300

    Abreviando document.querySeletor para diminiur código na chamada

--- javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,16 +1,24 @@
 class NegociacaoController {
 
-    adiciona(event){
-        event.preventDefault();
-        
-        let inputData = document.querySelector('#data');
-        let inputQuantidade = document.querySelector('#quantidade');
-        let inputValor = document.querySelector('#valor');    
-    
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);        
+    constructor() {
+
+        //bind mantém a associação do querySelector com o document
+        let $ = document.querySelector.bind(document);
+
+        this.inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+
     }
 
-        
+    adiciona(event) {
+
+        event.preventDefault();
+
+        console.log(this.inputData.value);
+        console.log(this.inputQuantidade.value);
+        console.log(this.inputValor.value);
+    }
+
+
 }
